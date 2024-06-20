f80ad55ddb077a3f9690add0d745f05a4668138f
arnavmehta7 <arnavmehta.cs@gmail.com>
2024-06-20 14:05:01 +0400

    hubconf updated

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,4 @@
-dependencies = ['torch', 'torchaudio', 'numpy', 'vocos', 'safetensors', 'huggingface_hub']
+dependencies = ['torch', 'torchaudio', 'numpy', 'vocos', 'safetensors']
 
 import logging
 import os
